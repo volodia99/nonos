685a9568729f07648ef6fcefcd7735f74214fb5d
volodia99 <ysengrin9@gmail.com>
2021-03-19 02:46:20 +0100

    Update __init__.py

--- nonos/__init__.py
+++ nonos/__init__.py
@@ -1,3 +1,3 @@
-from .main import readVTKPolar, AnalysisNonos, Parameters, Mesh, FieldNonos, PlotNonos, StreamNonos
+from .main import readVTKPolar, AnalysisNonos, InitParamNonos, Parameters, Mesh, FieldNonos, PlotNonos, StreamNonos
 
 __version__ = "0.0.3"
