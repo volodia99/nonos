6e2afee15a9391e5477d5b81c4046ff51c8f57d7
Gaylor WAFFLARD <gaylor.wafflard@univ-grenoble-alpes.fr>
2021-04-12 17:56:49 +0200

    added a test which checks if the corotation with a planet works.

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -28,3 +28,19 @@ def test_plot_simple_corotation(test_data_dir, capsys):
     # ignore differences in text wrapping because they are an implementation detail
     # due to the fact we use rich to display warnings
     assert err.strip().replace("\n", " ") == "Warning | We don't rotate the grid if there is no planet for now. omegagrid = 0."
+
+def test_plot_planet_corotation(test_data_dir):
+    from nonos import InitParamNonos, FieldNonos
+    from nonos.main import find_nearest
+
+    os.chdir(test_data_dir / "idefix_planet3d") 
+
+    init = InitParamNonos()
+    fieldon = FieldNonos(init, field='RHO', on=43)
+    azimfield=fieldon.data[find_nearest(fieldon.x,1.0),:,fieldon.imidplane]
+    assert find_nearest(azimfield, azimfield.max()) != fieldon.ny//2
+
+    initPlanet = InitParamNonos(isPlanet=True, corotate=True)
+    fieldonPlanet = FieldNonos(initPlanet, field='RHO', on=43)
+    azimfieldPlanet = fieldonPlanet.data[find_nearest(fieldonPlanet.x,1.0),:,fieldonPlanet.imidplane]
+    assert find_nearest(azimfieldPlanet, azimfieldPlanet.max()) == fieldonPlanet.ny//2
