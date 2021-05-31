4046a3aa05fbb60a0ca44fca724e5b536acde6ba
volodia99 <ysengrin9@gmail.com>
2021-05-31 12:22:41 +0200

    Update nonos/parsing.py
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- nonos/parsing.py
+++ nonos/parsing.py
@@ -61,9 +61,7 @@ def parse_range(extent, dim: int) -> Tuple[float, float, float, float]:
             raise ValueError(
                 f"Need to parse a range from sequence {extent} with exactly {2*dim} values."
             )
-        extent = [float(i) if i != "x" else None for i in extent]
-
-    return tuple(extent)
+        return = tuple(float(i) if i != "x" else None for i in extent)
 
 
 def range_converter(
