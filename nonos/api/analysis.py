287014cc5b5924d9b34ad7cab4124b91a8b45925
Volodia99 <gaylor.wafflard@univ-grenoble-alpes.fr>
2021-12-08 15:25:53 +0100

    heart transplant with a api (version 0.6.0)

--- nonos/api/analysis.py
+++ nonos/api/analysis.py
@@ -1220,3 +1220,69 @@ def from_data(
         on,
         operation=operation,
     )
+
+
+def temporal_all(
+    field: str,
+    operation: str,
+    onall,
+    directory: str = "",
+    planet_corotation: Optional[int] = None,
+):
+    datasum = 0
+    don = len(onall)
+    for on in sorted(onall):
+        datafield = from_file(
+            field=field, filename=operation, on=on, directory=directory
+        )
+        datafield_rot = datafield.rotate(planet_corotation=planet_corotation)
+        datasum += datafield_rot.data
+    datafieldsum = from_data(
+        field="".join([field, "T_ALL"]),
+        data=np.array(datasum / don),
+        coords=datafield.coords,
+        on=0,
+        operation="_" + operation,
+    )
+    return datafieldsum
+
+
+def temporal(
+    field: str,
+    operation: str,
+    onbeg: int,
+    *,
+    onend: Optional[int] = None,
+    directory: str = "",
+    planet_corotation: Optional[int] = None,
+):
+    datasum = 0
+    if onend is None:
+        datafield = from_file(
+            field=field, filename=operation, on=onbeg, directory=directory
+        )
+        datafield_rot = datafield.rotate(planet_corotation=planet_corotation)
+        datafieldsum = from_data(
+            field=field,
+            data=datafield_rot.data,
+            coords=datafield_rot.coords,
+            on=onbeg,
+            operation="_" + operation,
+        )
+        return datafieldsum
+    else:
+        don = onend - onbeg
+        for on in range(onbeg, onend + 1):
+            datafield = from_file(
+                field=field, filename=operation, on=on, directory=directory
+            )
+            datafield_rot = datafield.rotate(planet_corotation=planet_corotation)
+            datasum += datafield_rot.data
+        datafieldsum = from_data(
+            field="".join([field, f"T_{onbeg}_{onend}"]),
+            data=np.array(datasum / don),
+            coords=datafield.coords,
+            on=onend,
+            operation="_" + operation,
+        )
+        return datafieldsum
--- nonos/api/tools.py
+++ nonos/api/tools.py
@@ -1,8 +1,4 @@
-from typing import Optional
-
 import numpy as np
-
-from nonos.api.analysis import from_data, from_file
 
 
 def find_nearest(array, value):
@@ -19,69 +15,3 @@ def find_around(array, value):
     arraym = np.asarray(larray)
     idx_2 = (np.abs(arraym - value)).argmin()
     return np.asarray([array[idx_1], arraym[idx_2]])
-
-
-def temporal_all(
-    field: str,
-    operation: str,
-    onall,
-    directory: str = "",
-    planet_corotation: Optional[int] = None,
-):
-    datasum = 0
-    don = len(onall)
-    for on in sorted(onall):
-        datafield = from_file(
-            field=field, filename=operation, on=on, directory=directory
-        )
-        datafield_rot = datafield.rotate(planet_corotation=planet_corotation)
-        datasum += datafield_rot.data
-    datafieldsum = from_data(
-        field="".join([field, "T_ALL"]),
-        data=np.array(datasum / don),
-        coords=datafield.coords,
-        on=0,
-        operation="_" + operation,
-    )
-    return datafieldsum
-
-
-def temporal(
-    field: str,
-    operation: str,
-    onbeg: int,
-    *,
-    onend: Optional[int] = None,
-    directory: str = "",
-    planet_corotation: Optional[int] = None,
-):
-    datasum = 0
-    if onend is None:
-        datafield = from_file(
-            field=field, filename=operation, on=onbeg, directory=directory
-        )
-        datafield_rot = datafield.rotate(planet_corotation=planet_corotation)
-        datafieldsum = from_data(
-            field=field,
-            data=datafield_rot.data,
-            coords=datafield_rot.coords,
-            on=onbeg,
-            operation="_" + operation,
-        )
-        return datafieldsum
-    else:
-        don = onend - onbeg
-        for on in range(onbeg, onend + 1):
-            datafield = from_file(
-                field=field, filename=operation, on=on, directory=directory
-            )
-            datafield_rot = datafield.rotate(planet_corotation=planet_corotation)
-            datasum += datafield_rot.data
-        datafieldsum = from_data(
-            field="".join([field, f"T_{onbeg}_{onend}"]),
-            data=np.array(datasum / don),
-            coords=datafield.coords,
-            on=onend,
-            operation="_" + operation,
-        )
-        return datafieldsum
