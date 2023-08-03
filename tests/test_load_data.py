3c4eb7ddea46228e9d315a0feaad7c3b5cf35043
Clément Robert <cr52@protonmail.com>
2023-08-03 17:57:25 +0200

    TST: simplify test

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -28,11 +28,7 @@ def test_roundtrip_simple(test_data_dir):
 
 def test_roundtrip_other_dir(test_data_dir, tmp_path):
     os.chdir(test_data_dir / "idefix_spherical_planet3d")
-    ds = GasDataSet(500)
-    assert len(list(ds.keys())) == 7
-
-    gf = ds["RHO"].azimuthal_average()
-
+    gf = GasDataSet(500)["RHO"].azimuthal_average()
     gf.save(tmp_path)
     dsnpy = GasDataSet.from_npy(500, operation="azimuthal_average", directory=tmp_path)
     assert len(list(dsnpy.keys())) == 1
